77f294d2757fb5f79611e1ead1df12229a89c20e
Juan <juan.verao@udea.edu.co>
2022-11-22 21:47:49 -0500

    prueba

---
+++ Assets/Scripts/MiniScanner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniScanner : MonoBehaviour
+
+{
+    public float speed;
+    public float delayEndTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        endScanner();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vectorMesh = this.transform.localScale;
+        float growing = this.speed * Time.deltaTime;
+        this.transform.localScale = new Vector3(vectorMesh.x + growing, vectorMesh.y + growing, vectorMesh.z + growing);
+    }
+
+    private void endScanner()
+    {
+        Destroy(this.gameObject, delayEndTime);
+    }
+}
--- Assets/Scripts/ScannerController.cs
+++ Assets/Scripts/ScannerController.cs
@@ -7,7 +7,9 @@ public class ScannerController : MonoBehaviour
 {
     public float speed;
     public float delayEndTime;
-    public TagField objetivo;
+    public GameObject objective;
+    public GameObject scanner;
+    private GameObject activeScanner;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,5 +26,19 @@ public class ScannerController : MonoBehaviour
 
     private void endScanner(){
         Destroy(this.gameObject, delayEndTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Transform pos = objective.transform;
+        if (collision.gameObject.CompareTag(objective.tag))
+        {
+            Scan(pos);
+        }
+    }
+
+    public void Scan(Transform obj)
+    {
+        activeScanner = Instantiate(scanner, obj);
     }
 }
